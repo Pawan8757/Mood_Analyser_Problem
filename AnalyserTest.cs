4fac4ddd97c797ce2f41122f4942572b046c7595
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-03-11 23:35:59 +0530

    adding analysertest program

---
+++ AnalyserTest.cs
@@ -0,0 +1,40 @@
+﻿using MoodAnalayzer;
+
+namespace MoodAnalyzerTest
+{
+
+    public class AnalyzerTest
+    {
+        public object Assert { get; private set; }
+
+        
+        public void GivenSadMoodShouldReturnSAD()
+        {
+            // Arrange
+            string expected = "SAD";
+            string message = "I am in Sad Mood";
+            MoodAnalyse moodAnalyse = new MoodAnalyse();
+
+            // Act
+            string mood = moodAnalyse.AnalyseMood(message);
+
+            // Assert
+            Assert.AreEqual(expected, mood);
+        }
+
+       
+        public void GivenAnyMoodShouldReturnHAPPY()
+        {
+            // Arrange
+            string expected = "HAPPY";
+            string message = "I am in Any Mood";
+            MoodAnalyse moodAnalyse = new MoodAnalyse();
+
+            // Act
+            string mood = moodAnalyse.AnalyseMood(message);
+
+            // Assert
+            Assert.AreEqual(expected, mood);
+        }
+    }
+}
